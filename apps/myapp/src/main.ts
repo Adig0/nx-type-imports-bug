70b1da8ef2d98f0f05bdf2a8a3cfc6746f68aa4b
Adi Golan <u9324450@bsmch.net>
2025-06-17 01:25:06 +0300

    done

--- apps/myapp/src/main.ts
+++ apps/myapp/src/main.ts
@@ -1,5 +1,4 @@
 import type { ReactNode } from 'react';
-import { truncate } from 'lodash';
 
-let msg: ReactNode = 'Hello1';
-console.log(truncate(msg as string));
+let message: ReactNode = 'Hello World!';
+console.log(message);
